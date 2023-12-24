b11309527c19a41ba30b36738bf9e123bc6e39b0
neomond <neoweise@yahoo.com>
2023-12-24 13:27:51 +0400

    feat: share functionality with privacy access  were added

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController {
         
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
         if let imageToLoad = selectedImage{
             imageView.image = UIImage(named: imageToLoad)
@@ -32,6 +33,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image Found!")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
